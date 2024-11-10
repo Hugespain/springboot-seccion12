4f0cb6dcfca51d52e6f0a60b739281b89013fef7
Hugespain <diegosilva3795@gmail.com>
2024-11-10 19:08:10 +0100

    Controlador creado por mi mismo

--- src/main/java/com/andres/curso/springboot/app/springbootcrud/controllers/ProductController.java
+++ src/main/java/com/andres/curso/springboot/app/springbootcrud/controllers/ProductController.java
@@ -61,9 +61,9 @@ public ResponseEntity<Product> create(@RequestBody Product product){ //Voy a dev
 public ResponseEntity<Product> update (@PathVariable Long id, @RequestBody Product product){
     Optional<Product> productOptional = service.update(id, product);
     if (productOptional.isPresent()) {
-        return  ResponseEntity.status(HttpStatus.CREATED).body(productOptional);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(productOptional.get());
     }
-   return ResponseEntity.notFound();
+   return ResponseEntity.notFound().build();
 
 }
 }
